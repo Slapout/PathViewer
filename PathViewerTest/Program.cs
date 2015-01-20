f88a0d4f60edb703cf30a8f4dbb9c2d9780aa2c3
Slapout <markbrents@hotmail.com>
2015-01-19 20:41:00 -0600

    Initial commit

---
+++ PathViewerTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PathViewer; 
+
+namespace PathViewerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = EnvironmentData.GetEnvironmentFromRegistry("PATH");
+            Console.WriteLine("Path = {0}", result);
+        }
+    }
+}
